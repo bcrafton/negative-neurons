87c2ac457e38c19c4602ea7dfce4455d4281d8cb
Brian Crafton <crafton.b@husky.neu.edu>
2019-07-30 00:32:38 +0200

    using other model

---
+++ neg_equiv/v3/t6.py
@@ -0,0 +1,156 @@
+
+import argparse
+import os
+import sys
+
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=250)
+parser.add_argument('--batch_size', type=int, default=25)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--lr', type=float, default=1e-2)
+parser.add_argument('--eps', type=float, default=1e-6)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+import numpy as np
+import tensorflow as tf
+import keras
+from collections import deque
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+assert(np.shape(x_train) == (50000, 32, 32, 3))
+x_train = x_train - np.mean(x_train, axis=0, keepdims=True)
+x_train = x_train / np.std(x_train, axis=0, keepdims=True)
+y_train = keras.utils.to_categorical(y_train, 10)
+
+assert(np.shape(x_test) == (10000, 32, 32, 3))
+x_test = x_test - np.mean(x_test, axis=0, keepdims=True)
+x_test = x_test / np.std(x_test, axis=0, keepdims=True)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+####################################
+
+x = tf.placeholder(tf.float32, [None, 32 , 32 , 3])
+y = tf.placeholder(tf.float32, [None, 10])
+lr = tf.placeholder(tf.float32, ())
+
+####################################
+
+w = np.load('cifar10_weights.npy', allow_pickle=True).item()
+tconv1_weights = w['conv1_weights']
+tconv2_weights = w['conv2_weights']
+tconv3_weights = w['conv3_weights']
+
+tw1 = tf.Variable(tconv1_weights, dtype=tf.float32)
+tw2 = tf.Variable(tconv2_weights, dtype=tf.float32)
+tw3 = tf.Variable(tconv3_weights, dtype=tf.float32)
+
+w1p_init = np.random.normal(loc=np.average(tconv1_weights), scale=np.std(tconv1_weights), size=np.shape(tconv1_weights))
+w1n_init = np.random.normal(loc=np.average(tconv1_weights), scale=np.std(tconv1_weights), size=np.shape(tconv1_weights))
+w2p_init = np.random.normal(loc=np.average(tconv2_weights), scale=np.std(tconv2_weights), size=np.shape(tconv2_weights))
+w2n_init = np.random.normal(loc=np.average(tconv2_weights), scale=np.std(tconv2_weights), size=np.shape(tconv2_weights))
+w3_init = np.random.normal(loc=np.average(tconv3_weights), scale=np.std(tconv3_weights), size=np.shape(tconv3_weights))
+
+w1p = tf.Variable(w1p_init, dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+w1n = tf.Variable(w1n_init, dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+w2p = tf.Variable(w2p_init, dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+w2n = tf.Variable(w2n_init, dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+w3  = tf.Variable(w3_init,  dtype=tf.float32, constraint=lambda x: tf.clip_by_value(x, 0, np.infty))
+
+####################################
+
+def conv_op(x, w):
+    conv = tf.nn.conv2d(x, w, [1,1,1,1], 'SAME')
+    conv = tf.nn.relu(conv)
+    return conv
+    
+def conv_op_np(x, wp, wn):
+    convp = conv_op(x, wp)
+    convn = conv_op(x, wn)
+    conv  = tf.nn.relu(convp) - tf.nn.relu(convn)
+    return conv
+
+####################################
+
+conv1 = conv_op_np(x, w1p, w1n)
+pool1 = tf.nn.avg_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+conv2 = conv_op_np(pool1, w2p, w2n)
+pool2 = tf.nn.avg_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+conv3 = tf.nn.relu(tf.nn.conv2d(pool2, w3, [1,1,1,1], 'SAME'))
+pool3 = tf.nn.avg_pool(conv3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+####################################
+
+tconv1 = conv_op(x, tw1)
+tpool1 = tf.nn.avg_pool(tconv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+tconv2 = conv_op(tpool1, tw2)
+tpool2 = tf.nn.avg_pool(tconv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+tconv3 = conv_op(tpool2, tw3)
+tpool3 = tf.nn.avg_pool(tconv3, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+####################################
+
+loss = tf.losses.mean_squared_error(labels=tpool3, predictions=pool3)
+params = [w1p, w1n, w2p, w2n, w3]
+grads = tf.gradients(loss, params)
+grads_and_vars = zip(grads, params)
+
+train = tf.train.AdamOptimizer(learning_rate=lr, epsilon=args.eps).apply_gradients(grads_and_vars)
+
+####################################
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+####################################
+
+random_losses = []
+random_sign_matches = []
+
+for jj in range(0, 50000, args.batch_size):
+    s = jj
+    e = jj + args.batch_size
+    xs = x_train[s:e]
+    ys = y_train[s:e]
+    
+    [l, o, to] = sess.run([loss, pool3, tpool3], feed_dict={x: xs, y: ys, lr: 0.0})
+    
+    random_sign_match = np.sum(np.sign(o) == np.sign(to)) * 1.0 / np.prod(np.shape(o))
+    random_sign_matches.append(random_sign_match)
+    
+    random_losses.append(l)
+
+####################################
+
+for ii in range(args.epochs):
+    
+    losses = []
+    sign_matches = []
+    
+    for jj in range(0, 50000, args.batch_size):
+        s = jj
+        e = jj + args.batch_size
+        xs = x_train[s:e]
+        ys = y_train[s:e]
+        
+        [l, o, to, _] = sess.run([loss, pool3, tpool3, train], feed_dict={x: xs, y: ys, lr: args.lr})
+        
+        sign_match = np.sum(np.sign(o) == np.sign(to)) * 1.0 / np.prod(np.shape(o))
+        sign_matches.append(sign_match)
+        
+        losses.append(l)
+        
+    print ('loss %f/%f | match %f/%f' % (np.average(losses), np.average(random_losses), np.average(sign_matches), np.average(random_sign_matches)))
+        
+####################################
+        
